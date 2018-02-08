411c0d7428ee31f99a395f78e4948b4606a34db9
Satoshi Takahashi <s-takahashi@Satoshi-no-MacBook-Pro.local>
2018-02-09 00:14:27 +0900

    read db

--- push_heroku.py
+++ push_heroku.py
@@ -18,6 +18,17 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
+# モデル作成
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    line_userid = db.Column(db.String(80), unique=True)
+
+    def __init__(self, line_userid):
+        self.line_userid = line_userid
+
+    def __repr__(self):
+        return '<Users %r>' % self.line_userid
+
 
 # with open('line_id.csv') as f:
 #     reader = csv.reader(f)
