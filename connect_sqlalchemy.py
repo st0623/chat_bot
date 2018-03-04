41b2c31f93bf4f23848358dad22664169d09088c
Satoshi Takahashi <s-takahashi@Satoshi-no-MacBook-Pro.local>
2018-03-04 12:53:23 +0900

    enviroment

--- connect_sqlalchemy.py
+++ connect_sqlalchemy.py
@@ -2,24 +2,27 @@ import sqlalchemy as sa
 from sqlalchemy import *
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.sql import select 
+# 'postgresql://mvzgskipdsvaxp:6537b5845d6ce23156e60e7105bf3f9648baa097cf7208db697fcb56c53abc0e@ec2-174-129-26-203.compute-1.amazonaws.com:5432/d9ulhkpdiac7bc'
 
-def get_news_list():
-    url = 'postgresql://mvzgskipdsvaxp:6537b5845d6ce23156e60e7105bf3f9648baa097cf7208db697fcb56c53abc0e@ec2-174-129-26-203.compute-1.amazonaws.com:5432/d9ulhkpdiac7bc'
-    engine = sa.create_engine(url)
-    session = sessionmaker(bind=engine)()
-    conn = engine.connect()
+url = os.environ['NEWS_DATABASE_URL']
+engine = sa.create_engine(url)
+session = sessionmaker(bind=engine)()
+conn = engine.connect()
+ 
 
 # MetaData の生成
-    meta = MetaData()
+meta = MetaData()
+
 
 # Engineに結びつける
-    meta.bind = engine 
+meta.bind = engine 
 
-    sample_table = Table('news', meta, autoload=True)
+sample_table = Table('news', meta, autoload=True)
 # 全検索の定義および実行
-    selectQuery = select([sample_table])
-    executeResult = conn.execute(selectQuery).fetchall()
+selectQuery = select([sample_table])
+executeResult = conn.execute(selectQuery).fetchall()
 
-    return executeResult
-
+print (type(executeResult))
+# for row in executeResult:
+        # print(row)
 
