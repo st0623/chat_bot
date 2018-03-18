af36a502b41c7b21422fac7aa89a86d07465206e
Satoshi Takahashi <t4s3s5@gmail.com>
2018-03-18 16:20:33 +0900

    change db

--- push_heroku.py
+++ push_heroku.py
@@ -25,18 +25,6 @@ db = SQLAlchemy(app)
 
 sched = BlockingScheduler()
 
-# モデル作成
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    line_userid = db.Column(db.String(80), unique=True)
-
-    def __init__(self, line_userid):
-        self.line_userid = line_userid
-
-    def __repr__(self):
-        return '<Users %r>' % self.line_userid
-        
-# モデル作成
 class line_user(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     line_id = db.Column(db.String(255), unique=True)
